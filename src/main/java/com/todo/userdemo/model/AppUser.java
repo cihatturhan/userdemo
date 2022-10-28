6c10df3ad17c4e0bed48007a39857290a4a5ecce
Cihat TURHAN <cihatturhan@gmail.com>
2022-10-28 16:24:24 +0200

    Add dependencies Add application properties Add Entity features

--- src/main/java/com/todo/userdemo/model/AppUser.java
+++ src/main/java/com/todo/userdemo/model/AppUser.java
@@ -4,12 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name="appuser")
 public class AppUser {
 
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
     String username;
     String email;
